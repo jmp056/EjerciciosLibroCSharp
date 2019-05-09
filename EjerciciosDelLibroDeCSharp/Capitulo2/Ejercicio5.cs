9e999317cfb20de4e25723839e497aada04ce925
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-09 13:43:00 -0400

    Subiendo Solucion

---
+++ EjerciciosDelLibroDeCSharp/Capitulo2/Ejercicio5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosDelLibroDeCSharp.Capitulo2
+{
+    class Ejercicio5
+    {
+    }
+}
