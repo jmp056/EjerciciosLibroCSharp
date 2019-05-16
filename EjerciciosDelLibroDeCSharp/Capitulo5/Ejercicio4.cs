79367c10b69bc726944491fb0ddaaff834844876
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-16 00:06:46 -0400

    Ejercicios del capitulo 5 y el capitulo 6

---
+++ EjerciciosDelLibroDeCSharp/Capitulo5/Ejercicio4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosDelLibroDeCSharp.Capitulo5
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            factorial();
+            Console.ReadKey();
+        }
+
+        static void factorial()
+        {
+            string lector = "";
+            float n, r = 1;
+            Console.Write("Digite un numero: ");
+            lector = Console.ReadLine();
+            n = Convert.ToInt32(lector);
+            for (int x = 1; x <= n; x++)
+                r *= x;
+            Console.Write("El factorial de {0} es {1}", n, r);
+        }
+    }
+}
---
+++ EjerciciosDelLibroDeCSharp/Capitulo6/Ejercicio5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosDelLibroDeCSharp.Capitulo6
+{
+    class Ejercicio5
+    {
+        static void Main(string[] args)
+        {
+            int[][] numeros = new int[][]
+            {
+                new int[] { 9, 3, 1, 7, 2, 4 },
+                new int[] { 2, 9 },
+                new int[] { 3, 5, 2, 9 }
+            };
+            imprime(numeros);
+            Console.ReadKey();
+        }
+
+        static void imprime(int[][] numeros)
+        {
+            for (int x = 0; x  3; x++)
+            {
+                for (int y = 0; y  numeros[x].GetLength(0); y++)
+                    Console.Write({ 0}  , numeros[x][y]);
+                Console.Write(n);
+            }
+        }
+    }
+}
